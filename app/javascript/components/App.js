6f81c0fdf2f0aba225593ffb3cbb75e3424a6465
Luis Fernando <lfmnovaes@gmail.com>
2022-02-14 16:45:20 -0300

    Add App react interface

---
+++ app/javascript/components/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import HelloWorld from './HelloWorld'
+
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HelloWorld greeting="Friend" />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App
